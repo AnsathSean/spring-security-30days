e21ee2212cf6ba65d26287441d26b9c892c6a1b1
EricHsu <erichsu@perform-global.com>
2025-08-27 17:36:05 +0800

    重新更新資料

---
+++ spring-security-30days/src/main/java/com/ansathsean/security/SpringSecurity30daysApplication.java
@@ -0,0 +1,13 @@
+package com.ansathsean.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity30daysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity30daysApplication.class, args);
+	}
+
+}
